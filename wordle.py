a1b5497f5de55564f8d1d36920f6c9083a9118c5
Yu20010901 <yc318715@gmail.com>
2022-10-14 17:36:01 +0800

    complete answer checking

--- wordle.py
+++ wordle.py
@@ -4,3 +4,11 @@ if __name__ =="__main__":
 
     #answer
     answer = "apple"
+
+    for i in range(len(user_input)):
+        if user_input[i] == answer[i]:
+            print("A")
+        elif user_input[i] in answer:
+            print("B")
+        else:
+            print("X")
